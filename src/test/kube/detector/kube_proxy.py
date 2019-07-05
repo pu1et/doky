73fb08a2acda18731b18dada2161e6693f031c1b
pu1et <46569798+pu1et@users.noreply.github.com>
2019-07-05 21:01:59 +0900

    Update kube_proxy.py

--- src/test/kube/detector/kube_proxy.py
+++ src/test/kube/detector/kube_proxy.py
@@ -1,8 +1,39 @@
 import logging
 import requests
+import threading
+from subprocess import Popen
+import subprocess
+import time
+import __main__
+from src.types import Detector
 from src.conf.objects import OpenPortEvent, Service, Event
 from src.conf.abcd import works
-from src.types import Detector
+from src.test.kube.detector.hosts import AuthDetector
+
+
+class ProxyScanEvent(Event):
+    pass
+
+class ProxyScanEvent(Event):
+    def __init__(self, port):
+        self.port = port
+        self.path = ''
+
+
+@works.hang(ProxyScanEvent)
+class ProxyDetector(Detector):
+    def __init__(self,event):
+        self.event = event
+        self.port = self.event.port
+        self.host = self.event.host
+
+    def execute(self):
+        subprocess.Popen("kubectl proxy --port {}".format(self.port),shell=True)
+        logging.debug("proxy port {} start to open...".format(self.port))
+        self.host = '127.0.0.1'
+        time.sleep(0.5)
+        AuthDetector.connect_host("http",self.host,self.port)
+        subprocess.call(['fuser','-k','-n','tcp',str(self.port)])
 
 class KubeProxyEvent(Service, Event):
     def __init__(self):
@@ -25,3 +56,6 @@ class KubeProxy(Detector):
     def execute(self):
         if self.detect_kubeProxy():
             self.pick_point(KubeProxyEvent)
+            
+
+
