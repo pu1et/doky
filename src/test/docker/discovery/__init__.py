b565822ff665f4a227884c4efa4a7182a04f1121
pu1et <46569798+pu1et@users.noreply.github.com>
2019-07-06 05:11:42 +0900

    Delete __init__.py

--- src/test/docker/discovery/__init__.py
+++
@@ -1,6 +0,0 @@
-from os.path import dirname, basename, isfile
-import glob
-
-files = glob.glob(dirname(__file__)+"/*.py")
-for module_name in (basename(f)[:-3] for f in files if isfile(f) and not f.endswith('__init__.py')):
-    exec('from .{} import *'.format(module_name))
