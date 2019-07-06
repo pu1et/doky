1a2192eb52f5e9f008086cdde186177483678dd7
pu1et <46569798+pu1et@users.noreply.github.com>
2019-07-06 18:47:32 +0900

    Update doky.py

--- doky.py
+++ doky.py
@@ -10,8 +10,8 @@ try:
 except NameError:
     raw_input = input
 
-loglevel = "DEBUG"
-logging.basicConfig(level=logging.DEBUG, format='%(message)s', datefmt='%H:%M:%S')
+loglevel = "INFO"
+logging.basicConfig(level=logging.INFO, format='%(message)s', datefmt='%H:%M:%S')
 
 parser = argparse.ArgumentParser(description='Doky - scans your Docker , also Kubernetes')
 parser.add_argument('--details', action='store_true',help="scans the all pods in details")
@@ -19,6 +19,7 @@ parser.add_argument('-t','--token', type=str,help="scans on Worker Node using se
 parser.add_argument('-s','--service', type=str,help="scans only in a docker environment")
 parser.add_argument('-p','--proxy', type=int, help="access Master API opening available port")
 parser.add_argument('--docker', action='store_true', help="scans only in a docker environment")
+parser.add_argument('--temp', action='store_true', default=True, help="see result temporarily")
 
 
 options = parser.parse_args()
@@ -61,80 +62,97 @@ doky_work = False
 
 def main():
     global doky_work
-#    URL = "http://hotsix.kro.kr/re_result.php"
-#    intro = "\x1b[1;34m\n\n"
-#    intro += "    Dg.     qDi                             iQBBBBi :BB:                   dKDRBdu.              :BB  QBg          BBY\n"
-#    intro += "    BBr     BBP             KBv            BBQv7jQv  JJ                    BBBMQBBBBs            .BQ  iU.          72\n"
-#    intro += "    BB      BBj    sgBPr   jBBBjr         iBQ        :. ij.    uu          BQ:    .BBu    LRQU.  .BB   ..    LDBgi ...   .2BBK:   .vr   .vr   :IQBP:\n"
-#    intro += "    BQQDQQRbBBs  EBBSudBB: DBBBR5          KBBBs     BBi.QB:  BB7          BBr     7BB  SBBiiBBP  BB  BBB  EBBqUEX QBg  BQB1uQBB  DBB   dBB  QBdirgv\n"
-#    intro += "    BBRgQQMEBBv LBB    .BB  BBr              rQBQB.  BBi  ZBsBE            BBr     vBB iBBi::UBB  QB  PBB LBB      BQS BBP    1BB jBQ   jBB  BQg.\n"
-#    intro += "    BB      BBs gBB     BB  BB7                 vQB  QBi  .QBQi            BB:     BBr 5BB:i7r:i  BB  PBB QBg      BBU BBr    rBB UBB   LBB   :PBBBi\n"
-#    intro += "    BBi     BBI  BB7   BBM  BBE           vBr. .DBQ  BQr bBB PBB           BBM7YIBQB7   BBr      .QB  gBB .BBL   : BQE rBB.  .BBr iBB.  BBB  r   BBB\n"
-#    intro += "    BB:     BQs   PBBBBBr   :BBBb         .BBBBBBj   BB:JBQ   bBB          QBBBBBgr      PBBBBBv .BB  XBR   EBBBBg QB1  .QBBBQQ.   UBBBXXBB .BBQBBS\n\x1b[1;m"
-#    print(intro)
-#    print("\x1b[1;34m    ================================================================================================================================================\x1b[1;m")
-#    print("\x1b[1;34m    Hi, Kube-Six!\x1b[1;m")
-#    print("\x1b[1;34m    Kube-Six scans security weaknesses in Kubernetes clusters!\x1b[1;m")
-#    print("\x1b[1;34m    ================================================================================================================================================\n\x1b[1;m")
-#    print("\x1b[1;34m    write your email (ex. user@google.com)\x1b[1;m")
+    URL = "http://hotsix.kro.kr/test.php"
+    intro = "\x1b[1;34m\n\n"
+    intro += "    Dg.     qDi                             iQBBBBi :BB:                   dKDRBdu.              :BB  QBg          BBY\n"
+    intro += "    BBr     BBP             KBv            BBQv7jQv  JJ                    BBBMQBBBBs            .BQ  iU.          72\n"
+    intro += "    BB      BBj    sgBPr   jBBBjr         iBQ        :. ij.    uu          BQ:    .BBu    LRQU.  .BB   ..    LDBgi ...   .2BBK:   .vr   .vr   :IQBP:\n"
+    intro += "    BQQDQQRbBBs  EBBSudBB: DBBBR5          KBBBs     BBi.QB:  BB7          BBr     7BB  SBBiiBBP  BB  BBB  EBBqUEX QBg  BQB1uQBB  DBB   dBB  QBdirgv\n"
+    intro += "    BBRgQQMEBBv LBB    .BB  BBr              rQBQB.  BBi  ZBsBE            BBr     vBB iBBi::UBB  QB  PBB LBB      BQS BBP    1BB jBQ   jBB  BQg.\n"
+    intro += "    BB      BBs gBB     BB  BB7                 vQB  QBi  .QBQi            BB:     BBr 5BB:i7r:i  BB  PBB QBg      BBU BBr    rBB UBB   LBB   :PBBBi\n"
+    intro += "    BBi     BBI  BB7   BBM  BBE           vBr. .DBQ  BQr bBB PBB           BBM7YIBQB7   BBr      .QB  gBB .BBL   : BQE rBB.  .BBr iBB.  BBB  r   BBB\n"
+    intro += "    BB:     BQs   PBBBBBr   :BBBb         .BBBBBBj   BB:JBQ   bBB          QBBBBBgr      PBBBBBv .BB  XBR   EBBBBg QB1  .QBBBQQ.   UBBBXXBB .BBQBBS\n\x1b[1;m"
+    print(intro)
+    print("\x1b[1;34m    ================================================================================================================================================\x1b[1;m")
+    print("\x1b[1;34m    Hi, Kube-Six!\x1b[1;m")
+    print("\x1b[1;34m    Kube-Six scans security weaknesses in Kubernetes clusters!\x1b[1;m")
+    print("\x1b[1;34m    ================================================================================================================================================\n\x1b[1;m")
+    
+    email_input = raw_input("\x1b[1;34m    write your email (ex. user@google.com)\n    : \x1b[1;m")
 
-#    USER_TOKEN = raw_input("\x1b[1;34m    My Email: \x1b[1;m")
-#    if not "@" in USER_TOKEN:
-#        USER_TOKEN = USER_TOKEN.split("@")[0]
-#        print("check your email form:)")
-#        return
-#    else:
-#        email.set_email(USER_TOKEN)
-#        res = requests.post(URL, data={'chk':'0', 'token': USER_TOKEN})
-#        if not "1" in res.text:
-#            print("\x1b[1;34mThis email already exists.\n Please Try again.\x1b[1;m")
-#            return
-#        else:
-    global doky_work
-    all_options = [
-            options.details,
-            options.token,
-            options.proxy,
-            options.docker,
-            options.service
-            ]
-    try:
-        doky_lock.acquire()
-        doky_work = True
-        doky_lock.release()
+    if not "@" in email_input:
+        print("check your email form:)")
+        return
+    else:
+        res = requests.post(URL, data={'chk':'0.1', 'user_id': email_input})
+        check_exist = res.text
         
-        works.pick_point(DokyWorked())
-        
-        if not any(all_options) or options.details:
-            works.pick_point(HostEvent())
-            works.join()
-        
-        if options.docker:
-            works.pick_point(DockerScanEvent())
-            works.join()
-         
-        if options.service or options.token:
-            works.pick_point(AuthScanEvent())
-            works.join()
-       
-        if options.proxy:
-            works.pick_point(ProxyScanEvent(options.proxy))
-            works.join()
+        if options.temp:
+            temp = "O"
+        else: temp="X"
 
-    except KeyboardInterrupt:
-        logging.debug("KeyBoard Interrupt")
-    finally:
-        doky_lock.acquire()
-        if doky_work:
-            doky_lock.release()
-            if options.docker:
-                works.pick_point(DockerPrinter())
-            else:
-                works.pick_point(DokyPrinter())
-            works.join()
-            works.free()
+        if check_exist == "O": # email exists
+            print("\x1b[1;34m\n    ***Doky Login***\n\x1b[1;m")
         else:
+            print("\x1b[1;34m\n    ***Doky Join***\n\x1b[1;m")
+            check_email = raw_input("\x1b[1;34m    Insert you email: \x1b[1;m")
+            check_pass = raw_input("\x1b[1;34m    Insert you password: \x1b[1;m")
+            res = requests.post(URL, data={'chk':'0.2', 'user_id': check_email, 'user_pass':check_pass})
+            if res.text == "O": #login, join success
+                email.set_email(check_email)
+                if check_exist == "O":
+                    check = raw_input("\x1b[1;34m    Overwrite? \x1b[1;m")
+                    if check == "y" : check = "O"
+                    else: return
+                else: 
+                    res = requests.post(URL, data={'chk':'0.3', 'user_id': check_email, 'overwrite':"O", 'temp':temp})
+        
+
+        global doky_work
+        all_options = [
+                options.details,
+                options.token,
+                options.proxy,
+                options.docker,
+                options.service
+                ]
+        try:
+            doky_lock.acquire()
+            doky_work = True
             doky_lock.release()
+        
+            works.pick_point(DokyWorked())
+        
+            if not any(all_options) or options.details:
+                works.pick_point(HostEvent())
+                works.join()
+        
+            if options.docker:
+                works.pick_point(DockerScanEvent())
+                works.join()
+         
+            if options.service or options.token:
+                works.pick_point(AuthScanEvent())
+                works.join()
+       
+            if options.proxy:
+                works.pick_point(ProxyScanEvent(options.proxy))
+                works.join()
+
+        except KeyboardInterrupt:
+            logging.debug("KeyBoard Interrupt")
+        finally:
+            doky_lock.acquire()
+            if doky_work:
+                doky_lock.release()
+                if options.docker:
+                    works.pick_point(DockerPrinter())
+                else:
+                    works.pick_point(DokyPrinter())
+                works.join()
+                works.free()
+            else:
+                doky_lock.release()
 
 
 if __name__ == '__main__':
